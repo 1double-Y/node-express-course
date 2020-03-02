4a4ad86922b45fca426960adf1508678f804599b
yy_user <yy_user@qq.com>
2020-03-02 15:38:02 +0800

    add dynamic GET route

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const mockUserData=[
 app.get('/users',function(req,res){
 	res.json({
 		success: true,
-		message: 'successfully got users. Nice!',
-		users: mockUserData
+		message: 'got one user',
+		user: req.params.id
 	})
 })
 
